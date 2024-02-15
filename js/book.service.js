6bcd1da26c54f81d646223ee1527b814ac634044
bengold1999 <bengold1999@gmail.com>
2024-02-15 20:27:00 +0200

    Add remove func

--- js/book.service.js
+++ js/book.service.js
@@ -2,22 +2,50 @@
 var Gbooks
 getBooks()
 
-function getBooks(){
-Gbooks=[
-   getBook('jjk','80')
-]
+function getBooks() {
+    Gbooks = [
+        getBook('jjk', '80'),
+        getBook('jjk s2', '90')
+    ]
 }
 
 
-function getBook(title,price){
-    return{
+function getBook(title, price) {
+    return {
         id: makeId(),
         title,
         price,
-        imgUrl:'png'
+        imgUrl: 'png'
     }
 }
 
+
+function render() {
+    const strHtmls = Gbooks.map(book => ` 
+     <tr>
+    <td>${book.title}</td>
+    <td>${book.price}</td>
+    <td class="action-buttons">
+        <button>Read</button>
+        <button>Update</button>
+        <button onclick="onRemoveBook('${book.id}')">Delete</button>
+    </td>
+</tr>
+    `)
+
+    console.log(strHtmls.join(''))
+    const elRender = document.querySelector('tbody')
+    elRender.innerHTML = strHtmls.join('')
+
+}
+
+
+
+function removeBook(booId) {
+    const bookIdx = Gbooks.findIndex(book => book.id = booId)
+    Gbooks.splice(bookIdx,1)
+    render()
+}
 
 
 function makeId(length = 5) {
